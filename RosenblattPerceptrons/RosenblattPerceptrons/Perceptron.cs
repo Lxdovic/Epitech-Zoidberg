fb01f447e9c353a144ecea585cd2aebaf5a3181b
Lxdovic <ludovicdebever0@gmail.com>
2024-04-22 14:32:22 +0200

    feat(rosenblatt): implemented perceptron

--- RosenblattPerceptrons/RosenblattPerceptrons/Perceptron.cs
+++ RosenblattPerceptrons/RosenblattPerceptrons/Perceptron.cs
@@ -1,4 +1,23 @@
 namespace RosenblattPerceptrons;
 
 internal sealed class Perceptron {
+    public Perceptron(List<bool> inputs, List<double> weights, double threshold) {
+        if (inputs.Count != weights.Count) throw new ArgumentException("Inputs and weights must have the same length.");
+
+        Inputs = inputs;
+        Weights = weights;
+        Threshold = threshold;
+    }
+
+    public List<bool> Inputs { get; } = new();
+    public List<double> Weights { get; }
+    public double Threshold { get; set; }
+
+    public bool CalculateOutput() {
+        double sum = 0;
+
+        for (var i = 0; i < Inputs.Count; i++) sum += (Inputs[i] ? 1 : 0) * Weights[i];
+
+        return sum >= Threshold;
+    }
 }
--- RosenblattPerceptrons/RosenblattPerceptrons/Program.cs
+++ RosenblattPerceptrons/RosenblattPerceptrons/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace RosenblattPerceptrons;
 
-Console.WriteLine("Hello, World!");
+internal class Program {
+    private static void Main() {
+        var inputsWithLabels = new List<(string label, bool value)> {
+            ("Artists is Good", true),
+            ("Weather is Good", true),
+            ("Friend will Come", false),
+            ("Food is Served", true),
+            ("Alcohol is Served", true)
+        };
+
+        var inputs = new List<bool>();
+
+        foreach (var (_, value) in inputsWithLabels) inputs.Add(value);
+
+        var weights = new List<double> {
+            0.6, 0.5, 0.6, 0.2, 0.0
+        };
+
+        var threshold = 1.2;
+
+        var perceptron = new Perceptron(inputs, weights, threshold);
+
+        Console.WriteLine(perceptron.CalculateOutput());
+    }
+}
